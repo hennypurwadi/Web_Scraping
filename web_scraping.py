130a4beac65826e2dd392c673c265c9e6e8fd9e6
hennypurwadi <85436090+hennypurwadi@users.noreply.github.com>
2022-02-21 09:00:19 +0700

    Add files via upload

---
+++ web_scraping.py
@@ -0,0 +1,61 @@
+
+import requests 
+import pandas as pd 
+from bs4 import BeautifulSoup 
+import datetime
+from datetime import datetime, timedelta
+import time
+import schedule
+import h5py
+
+import warnings
+warnings.filterwarnings('ignore')
+
+today = datetime.now().date()
+today = str(today)
+
+file_url = pd.read_csv('url.csv')
+
+def web_scrape(url):
+    
+    Bsoup = BeautifulSoup(requests.get(url).text)
+    headers = [header.text for listing in Bsoup.find_all('thead') for header in listing.find_all('th')]
+    r_data = {header:[] for header in headers}
+
+    for rows in Bsoup.find_all('tbody'):
+          for row in rows.find_all('tr'):
+        
+            if len(row) != len(headers): continue
+            for idx, cell in enumerate(row.find_all('td')):
+                  r_data[headers[idx]].append(cell.text)
+    return pd.DataFrame(r_data)
+
+def job():
+    
+    #https://finance.yahoo.com/gainers
+    df0 = web_scrape(file_url['url_list'].iloc[0])
+    df00 = df0.iloc[:, 0:3]
+
+    #https://finance.yahoo.com/currencies
+    df1 = web_scrape(file_url['url_list'].iloc[1])
+    df01 = df1.iloc[:, 0:3]
+
+    #https://coinmarketcap.com/nft/collections/
+    df2 = web_scrape(file_url['url_list'].iloc[2])
+    df02 = df2.iloc[:, 0:5]
+
+    #https://finance.yahoo.com/cryptocurrencies/
+    df3 = web_scrape(file_url['url_list'].iloc[3])
+    df03 = df3.iloc[:, 0:3]
+    
+    #chosen url
+    df = df03 
+    
+    h5File = (today + '_web_scrape.h5')
+    df.to_hdf(h5File, 'w')
+    print("wrote hdf5 file done")   
+    
+schedule.every().day.at("10:30").do(job)
+
+if __name__ == "__main__":
+    job()
